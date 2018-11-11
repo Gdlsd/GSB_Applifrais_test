95cb8a841ffcacefd0a71b7ca087a8a71553d6dc
Gdlsd <dalessandro.guillaume@gmail.com>
2018-11-11 13:17:11 +0100

    08/11/18 Corrections : 	- La liste des visiteurs affichent seulement les visiteurs qui ont 		des fiches à valider 	- Les fiches validées ne sont plus accessible dans l'outil de 		validation. Un message s'affiche en signalant que la fiche a 		déjà été validée 	- Les données des frais hors forfait apparaissent désormais dans un 		tableau et non dans des champs de saisie.

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -10,14 +10,7 @@ $idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
 $idMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
 
 switch($action){
-    
-    case 'afficherListeVisiteurMois':
-        $lesVisiteurs = $pdo->getListeVisiteur();
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $idMois;
-        include 'vues/v_listeVisiteurs.php';
-        break;
-    
+       
     case 'corrigerFraisForfait' :
 
         $lesFrais = filter_input(INPUT_POST, 'lesFrais', 
@@ -30,38 +23,29 @@ switch($action){
         ajouterErreur('Les valeurs des frais doivent être numériques');
         include 'vues/v_erreurs.php';
         }             
-        $lesVisiteurs = $pdo->getListeVisiteur();
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $idMois;
-        include 'vues/v_listeVisiteurs.php';
         break;
         
     case 'validationFraisHorsForfait' :
         
-            $idMoisSuivant = getMoisSuivant($idMois);
-            $libelle = filter_input(INPUT_POST, 'libelleFraisHf', FILTER_SANITIZE_STRING);
-            $dateFrais = filter_input(INPUT_POST, 'dateFraisHf', FILTER_SANITIZE_STRING);
-            $montant= filter_input(INPUT_POST, 'montantFraisHf', FILTER_SANITIZE_STRING);
-            $idFrais = filter_input(INPUT_POST, 'idFrais', FILTER_SANITIZE_STRING);
+        $idMoisSuivant = getMoisSuivant($idMois);
+        $libelle = filter_input(INPUT_POST, 'libelleFraisHf', FILTER_SANITIZE_STRING);
+        $dateFrais = filter_input(INPUT_POST, 'dateFraisHf', FILTER_SANITIZE_STRING);
+        $montant= filter_input(INPUT_POST, 'montantFraisHf', FILTER_SANITIZE_STRING);
+        $idFrais = filter_input(INPUT_POST, 'idFrais', FILTER_SANITIZE_STRING);
             
         if(isset($_POST['repporterFiche']))
-        {
-
-        
+        {    
             if($pdo->estPremierFraisMois($idVisiteur, $idMoisSuivant)){
                 $pdo->creeNouvellesLignesFrais($idVisiteur, $idMoisSuivant);
                 $pdo->majEtatFicheFrais($idVisiteur, $idMois, 'CR'); // Le mois en cours est toujours en cours de création 
-            }
-                
+            }         
             $pdo->creeNouveauFraisHorsForfait(
             $idVisiteur,
             $idMoisSuivant,
             $libelle,
             $dateFrais,
             $montant
-                );
-
-            
+                );            
             $pdo->supprimerFraisHorsForfait($idFrais);        
 
                 ajouterSucces('Les frais on été repporté au mois suivant');
@@ -69,9 +53,8 @@ switch($action){
         }
         else
         {
-
             if(substr($libelle, 0, 8) !== 'REFUSE :'){
-                $libelleRefus = substr('REFUSE : ' . $libelle, 0, 20);
+                $libelleRefus = substr('REFUSE : ' . $libelle, 0, 40);
             $pdo->majLigneFraisHorsForfait($idFrais, $idVisiteur, $idMois, $libelleRefus, 
                    $dateFrais, $montant);
             ajouterSucces('Les frais pour "' . $libelle . '" on été refusés');
@@ -79,34 +62,49 @@ switch($action){
             }else{
                 ajouterErreur('Ce frais a déjà été refusé');
                 include 'vues/v_erreurs.php';
-            }
-            
-        }
-        
-        $lesVisiteurs = $pdo->getListeVisiteur();
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $idMois;
-        include 'vues/v_listeVisiteurs.php';
+            }   
+        } 
         break;
     
     case 'validerFicheFrais':
         
         $pdo->majEtatFicheFrais($idVisiteur, $idMois, 'VA');
-        
         ajouterSucces('La fiche de frais est validée');
         include 'vues/v_succes.php';
-        
-        $lesVisiteurs = $pdo->getListeVisiteur();
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $idMois;
-        include 'vues/v_listeVisiteurs.php';
         break;
 }
 
-$libelleEtat = $pdo->getLibelleEtat($idVisiteur, $idMois);
+//Affichage liste déroulantes lstVisiteur et lstMois
+$lesVisiteurs = $pdo->getListeVisiteur();
+$lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+$moisASelectionner = $idMois;
+include 'vues/v_listeVisiteurs.php';
+
+
+//affichage 
+$infosFiche = $pdo->getLesInfosFicheFrais($idVisiteur, $idMois);
+$libelleEtat = $infosFiche['libEtat'];
+$etatFiche = $infosFiche['idEtat'];
+$dateValidation = $infosFiche['dateModif'];
+
 $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $idMois);        
 $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $idMois);
 
-if(!empty($lesFraisForfait) || !empty($lesFraisHorsForfait)){   
-    include 'vues/v_validerFrais.php';
+
+if($etatFiche !== 'VA'){
+    if(!empty($lesFraisForfait) || !empty($lesFraisHorsForfait)){   
+        include 'vues/v_validerFrais.php';
+    }
+}else{
+    ajouterMessageInfo('La fiche de frais pour la période du ' . $numMois . '/' . $numAnnee . ' a été validée '
+            . ' le ' . dateAnglaisVersFrancais($dateValidation) . '.');
+    ajouterMessageInfo('Vous pouvez consulter cette fiche via l\'onglet "Suivre le paiement '
+            . 'des fiches de frais".');
+    
+    include 'vues/v_messageInfo.php';
+            
 }
+
+
+
+
