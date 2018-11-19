d53bc914ce18203354777cee319e24527ef93819
Gdlsd <dalessandro.guillaume@gmail.com>
2018-11-19 07:27:12 +0100

    19/11/18 Modification mineure 		-> Page de suivi de paiement

---
+++ controleurs/c_suivrePaiementFiche.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Gestion du suivi du paiement des fiches de frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Guillaume D'ALESSANDRO <dalessandro.guillaume@gmail.com>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+
+echo 'Bonjour';
